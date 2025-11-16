0ca0f9b85dc8eddb629833a115e0c8b20e3fb2bc
itsDGFari <danicamelecio8@gmail.com>
2025-11-16 14:23:34 +0800

    Update

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -35,7 +35,7 @@ const Career = () => {
             <div className="career-info-in">
               <div className="career-role">
                 <h4>Digital Business Student</h4>
-                <h5>Lithan Singapore</h5>
+                <h5>Lithan Academy Singapore</h5>
               </div>
               <h3>2025</h3>
             </div>
